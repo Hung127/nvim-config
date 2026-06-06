afe338e5f05abb0d8b493591ec3aa5697bb8ac6c
Hung127 <hunghungtt05@gmail.com>
2026-06-07 01:02:34 +0700

    make catpuccin as theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,22 @@
 return {
-    
+    'catppuccin/nvim',
+    priority = 1000, -- make priority to the most
+    config = function()
+        require('catppuccin').setup {
+        background = {
+          light = 'latte',
+          dark = 'mocha',
+        },
+        color_overrides = {
+          mocha = {
+            base = '#090d12',
+            mantle = '#030508',
+            crust = '#010203',
+          },
+        },
+      }
+
+        -- load colorscheme after config
+        vim.cmd.colorscheme 'catppuccin'
+    end
 }

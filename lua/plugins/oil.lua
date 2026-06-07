25f1271c3bd30fefea959c70e9e92e3d07e8b164
Hung127 <hunghungtt05@gmail.com>
2026-06-07 20:57:42 +0700

    feat: added __pycache__ to hidden list

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,6 +9,10 @@ return {
       default_file_explorer = true,
       view_options = {
         show_hidden = false,
+        is_hidden_file = function(name, bufnr)
+          -- Hide __pycache__ and standard hidden files (starting with a dot)
+          return vim.startswith(name, ".") or name == "__pycache__"
+        end,
       },
     },
     keys = {

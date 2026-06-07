057b98fe2737c84f691a3c855d179b6a747781f5
Hung127 <hunghungtt05@gmail.com>
2026-06-07 16:32:46 +0700

    feat: removed unecessary lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -207,85 +207,20 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {
-          cmd = {
-            'clangd',
-            '--background-index',
-            '--clang-tidy',
-            '--header-insertion=iwyu',
-            '--completion-style=detailed',
-            '--function-arg-placeholders',
-            '--fallback-style=file',
-            -- Performance optimizations to prevent freezing
-            '--limit-results=50',
-            '--limit-references=100',
-            '--pch-storage=memory',
-            -- Use compile_commands.json for proper C++23 support
-            '--compile-commands-dir=.',
-            '--query-driver=/usr/bin/g++',
-            -- Reduce background activity
-            '--background-index-priority=low',
-            '--all-scopes-completion=false',
-            -- Enable diagnostics with clang-tidy
-            '--enable-config',
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-            compilationDatabasePath = '.',
-          },
-          capabilities = vim.tbl_deep_extend('force', capabilities, {
-            textDocument = {
-              completion = {
-                completionItem = {
-                  snippetSupport = true,
-                  resolveSupport = {
-                    properties = { 'documentation', 'detail' },
-                  },
-                },
-              },
-            },
-          }),
-        },
+        clangd = {},
         marksman = {},
-        pyright = {
-          settings = {
-            python = {
-              pythonPath = vim.fn.getcwd() .. '/.venv/bin/python',
-              analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = 'workspace',
-                typeCheckingMode = 'basic',
-              },
-            },
-          },
-        },
+        pyright = {},
         jdtls = {},
-        -- sqls = {},
-        -- gopls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- Tell the language server to recognize the 'vim' global, no warning anymore
+                globals = { 'vim' },
+              },
             },
           },
         },
